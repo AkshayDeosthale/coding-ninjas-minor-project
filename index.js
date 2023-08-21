5545fa64abdad10e27f072c6be906dfccc4be7a0
AkshayDeosthale <akshay.deosthale6@gmail.com>
2023-08-21 14:00:41 +0530

    Initial commit

--- index.js
+++ index.js
@@ -2,22 +2,36 @@ const express = require("express");
 const app = express();
 const crypto = require("crypto");
 const path = require("path");
+const mongoose = require("mongoose");
 const port = 8000;
 
 let tempTodos = [];
 
 app.use(express.urlencoded());
 
-// app.set("views", __dirname + "/views");
-// app.set("view engine", "jsx");
-// app.engine("jsx", require("express-react-views").createEngine());
-
+//template setup
 app.use("/public", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Connect to the MongoDB database
+mongoose.connect(
+  "mongodb+srv://akshay:akshay@cluster0.rns1o.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+//API
 app.get("/", (req, res) => {
-  //   res.render("TodoPage", { tempTodos });
   res.render("todopage", { tempTodos });
 });
 
